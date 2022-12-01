90fc13a03721ba14b4afe16cab8a88c572233b28
frikit <osipovvictor1994@gmail.com>
2022-12-01 18:16:58 +0000

    solved day 1(simplified)

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -2,14 +2,12 @@ package day1
 
 import file.FileReader
 
-fun main(args: Array<String>) {
+fun main() {
     val fileContent = FileReader.readContent("/day1/input.txt")
 
     val top = fileContent.split("\n\n")
         .map { it.split("\n").filter { it.isNotBlank() } }
         .map { it.map { n -> n.toInt() }.sum() }
-        .sorted()
-        .reversed()
-        .first()
+        .maxOf { it }
     println(top)
 }
